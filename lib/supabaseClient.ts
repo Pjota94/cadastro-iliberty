4fc1ffb82b6a72650d553189c4a10a58a4d99cac
Paulo Junior <98767739+Pjota94@users.noreply.github.com>
2025-04-30 14:29:06 +0000

    add key and url supabase

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  "https://xeoqsvidfwsudiwiliev.supabase.co";
+const supabaseKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhlb3FzdmlkZndzdWRpd2lsaWV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyMTkzODIsImV4cCI6MjA1NDc5NTM4Mn0.UWLVcHtVoQJ50AdtAa2eeGfSE-Avf47Z5VKXeesr_5w";
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
